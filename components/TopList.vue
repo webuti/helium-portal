20bf4e76a671790821fb902cc5b493dfe80bf7fb
omer <qmerfp@me.com>
2021-11-26 16:49:11 +0300

    fix

--- components/TopList.vue
+++ components/TopList.vue
@@ -87,7 +87,7 @@
               <span @click="rewardOptions = !rewardOptions">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  class="h-6 w-6"
+                  class="h-6 w-6 ml-2"
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
